bc33a26d48daeb37f5abccf0d3453914b168bace
hanxu317317 <hanxu317@qq.com>
2017-09-27 18:14:52 +0800

    youhua

--- src/server/public/js/jiaohu.js
+++ src/server/public/js/jiaohu.js
@@ -90,7 +90,7 @@ $(document).ready(function() {
          }
       }
     });
-    $("#confirm-answer").on("tap", function() {
+    $(document).on('touchstart', '#confirm-answer', function() {
 
       if (questionIndex ==4) {
         initOver();
