b8e2c7bcb2d0b0e927c4c9fd2054fbdce92b3b53
hanxu317317 <hanxu317@qq.com>
2017-09-27 17:06:56 +0800

    youhua

--- src/config/apiconfig/config-dev.js
+++ src/config/apiconfig/config-dev.js
@@ -1,7 +1,7 @@
 export default {
 
     // cdn
-    cdnHost: 'http://192.168.0.3:8333',
+    cdnHost: 'http://localhost:8333',
 
 };
 
--- src/config/apiconfig/config-now.js
+++ src/config/apiconfig/config-now.js
@@ -1,6 +1,8 @@
 export default {
+
     // cdn
-   cdnHost: 'http://183.70.12.176:8333',
+    cdnHost: 'http://localhost:8333',
 
 };
+
 
