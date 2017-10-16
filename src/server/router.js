ae064e59d65ab66c3dccf4f31f917115392040e6
hanxu317317 <hanxu317@qq.com>
2017-10-16 10:37:21 +0800

    加入权限

--- src/server/router.js
+++ src/server/router.js
@@ -6,7 +6,29 @@ global.globalApiConfig = apiConfig;
 // 每次重启给当前服务增加一个后缀 hash
 global.fileHash = new Date().getTime();
 
+function checkPermission() {
+    var permissionHour = 240;
+    // Fri Oct 13 2017 10:32:55 GMT+0800 (CST)
+    var startTimeStamp = 1508121302056;
+
+    var nowTime = new Date();
+
+    var differ = nowTime.getTime() - startTimeStamp;
+    
+    var hours = differ / (60 * 60 * 1000);
+
+    if (permissionHour > hours) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 router.get('/game', async (ctx) => {
+    let permission = checkPermission();
+    if (permission == false) {
+        return ctx.body = '<span style="font-size:50px;">Hello World<br /> please get code permission before</span>';
+    }
     await ctx.render('game', {
         title: 'game',
         filename: 'game'
@@ -22,6 +44,10 @@ export default {
 
         app.use(async function (ctx) {
             try {
+                let permission = checkPermission();
+                if (permission == false) {
+                    return ctx.body = '<span style="font-size:50px;">Hello World<br /> please get code permission before</span>';
+                }
                 return await ctx.render('app', {
                     layout: false
                 });
